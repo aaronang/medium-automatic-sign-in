ffb10a207cf4e191cea2f4c0ea7ceae18b9526fb
Aaron Ang <awz.ang@gmail.com>
2018-07-03 00:58:48 -0700

    Fix automatic sign-in

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const button = document.querySelector(".metabar a.js-signInButton");
-if (button) button.click();
+function clickGoogleButton() {
+  setTimeout(() => {
+    const googleButton = document.querySelector(".js-googleButton");
+    if (googleButton) googleButton.click();
+  }, 1);
+}
+
+function handleNodeInserted(e) {
+  if (e.target.classList.contains("overlay")) {
+    clickGoogleButton();
+  }
+}
+
+const body = document.querySelector("body");
+body.addEventListener("DOMNodeInserted", handleNodeInserted);
+
+const userButton = document.querySelector(".js-userActions");
+if (!userButton) {
+  const button = document.querySelector(".metabar .js-signInButton");
+  if (button) setTimeout(() => button.click(), 1);
+}
+
+clickGoogleButton();
